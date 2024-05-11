1a0cf2799d2f8853abb4263170b7245f4deb0885
Joseph Walker <jlwalker@ucsd.edu>
2024-05-10 18:08:21 -0700

    graphical SDK working

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Header, HTTPException
 from fastapi.responses import JSONResponse, StreamingResponse
 from fastapi.encoders import jsonable_encoder
 from tensorflow.keras.preprocessing import image
@@ -12,6 +13,7 @@ import os
 import shutil
 import sys
 import json
+import uuid  # Import UUID library
 
 ## for seg3dtest
 #import app.ls_seg3d as ls_seg3d              # app. prefix is needed for running docker
@@ -20,6 +22,12 @@ import ls_seg3d as ls_seg3d
 print(tf.__version__)
 app = FastAPI()
 
+def validate_uuid(uuid_str: str) -> bool:
+    try:
+        uuid.UUID(uuid_str)
+        return True
+    except ValueError:
+        return False
 
 def predict(img):
     # Load the MobileNetV2 model (assuming it's in the same directory)
@@ -59,8 +67,12 @@ async def prediction(file: UploadFile = File(...)):
 
 
 @app.post("/seg3dtest")
-async def seg3dtest(uploaded_file: UploadFile = File(...)):
+async def seg3dtest(uploaded_file: UploadFile = File(...),
+                    uuid: str = Header(None)):
     try:
+        #if uuid is None or not validate_uuid(uuid):
+        #    raise HTTPException(status_code=400, detail="Invalid or missing UUID")
+        print("Received UUID server: ", str(uuid))
         # await uploaded_file.read()
         path = f"{uploaded_file.filename}"
         with open(path, 'w+b') as file:
@@ -72,8 +84,22 @@ async def seg3dtest(uploaded_file: UploadFile = File(...)):
         print("labels size info: ", labels.shape, labels.dtype, labels.nbytes)
         data = io.BytesIO(labels.tobytes())
         response = StreamingResponse(data, media_type = 'application/octet-stream')
+        #request_uuid = uuid.uuid4()
+        # Add the UUID as a custom header
+        response.headers['X-Response-UUID'] = str(uuid)
+
+        # Return the StreamingResponse object
         return response
-    
+        '''
+        # Generate a UUID for the request
+        request_uuid = uuid.uuid4()
+
+        # Return the streaming response with UUID
+        return {
+            "uuid": str(request_uuid),  # Convert UUID to string
+            "response": response
+        }
+        '''
     except Exception as e:
         if os.path.exists('tmp'):
             os.system('rm -r tmp')
